a362d4c93224589f63416dbd9d9e9a69b45335db
braylencunningham <braylen.cunningham@hgs.hiddengeniusproject.org>
2026-06-15 15:01:15 -0700

    Updated Content View File
    - Practice Project still in progress

--- PracticeProjectA/PracticeProjectA/ContentView.swift
+++ PracticeProjectA/PracticeProjectA/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName:
+                    "arcade.stick")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Braylen!")
+                .font(.largeTitle)
         }
         .padding()
     }
