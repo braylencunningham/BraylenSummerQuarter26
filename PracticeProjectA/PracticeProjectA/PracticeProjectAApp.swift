12e66cb7b9896acbf1eaaa781c1c45a4b5bc664b
braylencunningham <braylen.cunningham@hgs.hiddengeniusproject.org>
2026-06-15 14:29:39 -0700

    Uploaded Practice Project A
    - Created an empty Xcode project

---
+++ PracticeProjectA/PracticeProjectA/PracticeProjectAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeProjectAApp.swift
+//  PracticeProjectA
+//
+//  Created by Braylen Cunningham on 6/15/26.
+//
+
+import SwiftUI
+
+@main
+struct PracticeProjectAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
